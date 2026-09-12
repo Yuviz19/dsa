5d9e542702e9b32518083bce1fc51e6b54f41233
ByteVoid <es.yuviz19@gmail.com>
2026-09-12 18:03:08 +0530

    dsa: kth missing number

--- striver/04_binarySearch/02_BSANS/07_kthMissing.cpp
+++ striver/04_binarySearch/02_BSANS/07_kthMissing.cpp
@@ -1,58 +1,33 @@
 // find the kth missing number from the sorted increasing array
-#include <algorithm>
-#include <map>
 #include <vector>
 
-int brute(std::vector<int> &v, int k) {
-  std::map<int, int> map;
-  int max = *std::max_element(v.begin(), v.end());
-
-  for (auto i : v) {
-    map[i]++;
-  }
-
-  std::vector<int> missing;
-  for (int i = 1; i <= max + k; i++) {
-    if (map[i] == 0) {
-      missing.push_back(i);
-    }
-  }
-
-  return missing[k - 1];
-}
-
-// would recommend to watch the vid again
-int findKthPositive(std::vector<int> &arr, int k) {
-  int n = arr.size();
-  if (n == 0)
-    return k;
-  // find the missing number using binary search
-
-  int low = 0, high = n - 1;
-  while (low <= high) {
-    int mid = (low + high) / 2;
-    // missing number at mid
-    int missing = arr[mid] - (mid + 1);
-    if (missing < k)
-      low = mid + 1;
-    else
-      high = mid - 1;
-  }
-
-  return low + k;
-  // can also 'return high + 1 + k'
-}
-
-int bruteByStriver(std::vector<int> &v, int k) {
-
-  for (int i = 0; i < v.size(); i++) {
-    if (v[i] <= k)
+// brute force method
+int brute(std::vector<int> &arr, int k) {
+  int ans = k;
+  for (int i = 0; i < arr.size(); i++) {
+    if (arr[i] <= k) {
       k++;
-    else
+    } else
       break;
   }
 
-  return k;
+  return ans;
+}
+
+// using binary search
+int optimal(std::vector<int> &arr, int k) {
+  int low = 0, high = arr.size() - 1;
+  while (low <= high) {
+    int mid = (low + high) / 2;
+    int missingAtMid = arr[mid] - (mid + 1);
+
+    if (missingAtMid < k) {
+      low = mid + 1;
+    } else
+      high = mid - 1;
+  }
+
+  return (k + high + 1);
 }
 
 int main(int argc, char *argv[]) {
